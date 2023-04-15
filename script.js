f930c62c1197dcc708ee259ad54250d6b7e6ff0d
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-15 16:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let list = document.querySelectorAll('strong');
+	list.forEach((element,index)=>{
+		element.style.color = 'green';
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let list = document.querySelectorAll('strong');
+	list.forEach((element,index)=>{
+		element.style.color = 'black';
+	});
     
 }
